d6d00fa21b10f8b48eb68f480c80f74324aa860e
jaheine-deloitte <jaheine@deloitte.de>
2024-08-11 19:45:13 +0200

    Add socials

--- src/app/pages/home/sections/NextSection.tsx
+++ src/app/pages/home/sections/NextSection.tsx
@@ -1,21 +1,37 @@
 /* eslint-disable react/no-unescaped-entities */
+import Image from "next/image";
+import { Socials } from "./Socials";
+
 export function NextSection() {
   return (
-    <div id="next-section" className="flex items-center justify-center w-full h-screen">
+    <div
+      id="next-section"
+      className="flex items-center justify-center w-full h-screen"
+    >
       <div className="p-4 sm:p-6 md:p-8 lg:p-10 w-full max-w-5xl h-auto">
         <p className="text-left text-white text-xl sm:text-2xl">
           Welcome to My Homepage
-          <br/><br/>
-          Hello! I'm Jakob, a 26-year-old living in the beautiful city of Dresden with my fiancée and my dog. I work for Deloitte, specializing in product development on AWS.
-          <br/><br/>
-          This website is still a work in progress. In true Continuous Deployment fashion, I’ll be updating it live as I make changes—so keep an eye out for new content! :P
-          <br/><br/>
-          I'm currently busy planning a wedding and enjoying the summer. More exciting updates will come as soon as I find the time!
-          <br/><br/>
+          <br />
+          <br />
+          Hello! I'm Jakob, a 26-year-old living in the beautiful city of
+          Dresden with my fiancée and my dog. I work for Deloitte, specializing
+          in product development on AWS.
+          <br />
+          <br />
+          This website is still a work in progress. In true Continuous
+          Deployment fashion, I’ll be updating it live as I make changes—so keep
+          an eye out for new content! :P
+          <br />
+          <br />
+          I'm currently busy planning a wedding and enjoying the summer. More
+          exciting updates will come as soon as I find the time!
+          <br />
+          <br />
           So Long,
-          <br/>
+          <br />
           Jakob
         </p>
+        <Socials/>
       </div>
     </div>
   );
---
+++ src/app/pages/home/sections/Socials.tsx
@@ -0,0 +1,56 @@
+/* eslint-disable react/no-unescaped-entities */
+import Image from "next/image";
+
+export function Socials() {
+  return (
+    <div className="flex justify-center mt-20">
+      <div className="flex space-x-20">
+      <div className="w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20">
+          <a
+            href="https://linkedin.com/in/jakob-heine"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/svgs/linked-in.svg"
+              alt="LinkedIn"
+              width={80}
+              height={80}
+              className="w-full h-full"
+            />
+          </a>
+        </div>
+        <div className="w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20">
+          <a
+            href="https://github.com/jakobheine"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/svgs/github.svg"
+              alt="GitHub"
+              width={80}
+              height={80}
+              className="w-full h-full"
+            />
+          </a>
+        </div>
+        <div className="w-12 h-12 sm:w-16 sm:h-16 md:w-20 md:h-20">
+          <a
+            href="https://instagram.com/_jakobheine"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/svgs/instagram.svg"
+              alt="Instagram"
+              width={80}
+              height={80}
+              className="w-full h-full"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
