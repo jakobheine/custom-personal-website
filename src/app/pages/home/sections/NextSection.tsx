7b6b2d92660338bbe6b038efd61ae7409660abad
Jakob Heine <jakobheine>
2024-12-13 23:10:21 +0100

    feat(next): Leak cool new job :P

--- src/app/pages/home/sections/NextSection.tsx
+++ src/app/pages/home/sections/NextSection.tsx
@@ -29,8 +29,10 @@ export function NextSection() {
           <br />
           <br />
           Hello! I'm Jakob, {age} years old, living in the beautiful city of
-          Dresden with my fiancée and my dog. 
-          {/* I work as Software Development Engineer for Amazon. */}
+          Dresden with my fiancée and my dog.
+          <br />
+          <br />
+          I work as a Software Development Engineer for Amazon.
           <br />
           <br />
         </p>
